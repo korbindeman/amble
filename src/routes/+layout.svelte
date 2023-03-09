21ed56007d640ba49c4c986bf719dbc74092d69c
Korbin de Man <113640462+korbindeman@users.noreply.github.com>
2023-03-09 01:53:01 +0100

    feat: created page layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../app.css';
+
+	import Navigation from '$lib/components/Navigation.svelte';
+</script>
+
+<div class="flex justify-center mt-32 min-h-screen">
+	<div>
+		<Navigation />
+		<div class="grid grid-cols-2 gap-4">
+			<slot />
+		</div>
+	</div>
+</div>
