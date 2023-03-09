1ba17c72cfe31d9f0fe3c41793e75c17603b4295
Korbin de Man <113640462+korbindeman@users.noreply.github.com>
2023-03-09 01:53:21 +0100

    feat: created the root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Calendar from '$lib/components/Calendar.svelte';
+	import Card from '$lib/components/Card.svelte';
+</script>
+
+<Card>
+	<Calendar />
+</Card>
